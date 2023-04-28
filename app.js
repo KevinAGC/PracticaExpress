9ae225f6c0c794fc19941b9f5ea6b329a60e51dd
Kevin Abel Garcia Carrasco <kevin2001garciacarrasco@gmail.com>
2023-04-28 09:47:29 -0600

    Servidor con bodyparser

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var app = express();
+//Require packages and set the port
+const express = require('express');
+const port = 3002;
+//para permitir manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
 
-app.get('/',function(req,res){
-    res.send('Una API basica desde Express');
-});
+//Usar Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
-app.get('/saludo',function(req,res){
-    res.send('Hola desde la API');
-});
+routes(app);
 
-app.get('/despedida',function(req,res){
-    res.send('Adios desde la API');
-});
+//Iniciar el servidor
+const server = app.listen(port, (error)=> {
+    if(error) return console.log(`Error: ${error}`);
 
-app.listen(3000,function(){
-    console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
-});
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
+})
