f6976968ebf40a3782c3df81a5a70acffc7c296a
Kevin Abel Garcia Carrasco <kevin2001garciacarrasco@gmail.com>
2023-05-08 09:55:24 -0600

    Tabla Productos

--- routes/routes.js
+++ routes/routes.js
@@ -36,10 +36,14 @@ const router = (app) => {
 
   app.put("/users/:id", (request, response) => {
     const id = request.params.id;
-    pool.query("UPDATE usuarios SET ? WHERE id = ?", [request.body,id], (error, result) => {
-      if (error) throw error;
-      response.status(201).send(`User updated succesfully`);
-    });
+    pool.query(
+      "UPDATE usuarios SET ? WHERE id = ?",
+      [request.body, id],
+      (error, result) => {
+        if (error) throw error;
+        response.status(201).send(`User updated succesfully`);
+      }
+    );
   });
 
   app.delete("/users/:id", (request, response) => {
@@ -50,6 +54,51 @@ const router = (app) => {
     });
   });
 
+  //PRODUCTOS
+
+  //Mostrar todos los productos
+  app.get("/products", (request, response) => {
+    pool.query("SELECT * FROM productos", (error, result) => {
+      if (error) throw error;
+
+      response.send(result);
+    });
+  });
+
+  app.get("/products/:id", (request, response) => {
+    const id = request.params.id;
+    pool.query("SELECT * FROM productos WHERE id = ?", id, (error, result) => {
+      if (error) throw error;
+      response.send(result);
+    });
+  });
+
+  app.post("/products", (request, response) => {
+    pool.query("INSERT INTO productos SET ?", request.body, (error, result) => {
+      if (error) throw error;
+      response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+  });
+
+  app.put("/products/:id", (request, response) => {
+    const id = request.params.id;
+    pool.query(
+      "UPDATE productos SET ? WHERE id = ?",
+      [request.body, id],
+      (error, result) => {
+        if (error) throw error;
+        response.status(201).send(`User updated succesfully`);
+      }
+    );
+  });
+
+  app.delete("/products/:id", (request, response) => {
+    const id = request.params.id;
+    pool.query("DELETE FROM productos WHERE id = ?", id, (error, result) => {
+      if (error) throw error;
+      response.status(201).send(`User deleted succesfully`);
+    });
+  });
 };
 
 module.exports = router;
